4839cf8a01d9539d870aece57ed9742f9e863113
tavito7 <139911066+LuizOtavioADS@users.noreply.github.com>
2023-10-28 22:47:29 -0300

    code
    code

--- script.js
+++ script.js
@@ -9,10 +9,30 @@ function verificar(){
         let fsex = document.getElementsByName('radsex')
         let idade = ano - Number(fano.value)
         let genero = ''
+        let img = document.createElement('img')
+        img.setAttribute('id','foto')
         if (fsex[0].checked){
             genero = 'Homem'
+            if(idade >= 0 && idade < 10){
+                //criança
+            } else if (idade >= 10 && idade < 21){
+                //Jovem
+            } else if (idade < 50){
+                //adulto
+            } else {
+                //idoso
+            }
         }else if (fsex[1].checked){
             genero = 'Mulher'
+            if(idade >= 0 && idade < 10){
+                //criança
+            } else if (idade >= 10 && idade < 21){
+                //Jovem
+            } else if (idade < 50){
+                //adulto
+            } else {
+                //idoso
+            }
         }
         res.style.textAlign = 'center'
         res.innerHTML = `${genero} com ${idade} anos.`
